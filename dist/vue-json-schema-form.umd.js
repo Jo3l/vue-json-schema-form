5f3da22863b340fa3b9641435faaa9a151df78db
Jo3l <enriquito@gmail.com>
2020-11-20 10:59:16 +0100

    wip

--- dist/vue-json-schema-form.umd.js
+++ dist/vue-json-schema-form.umd.js
@@ -2305,13 +2305,6 @@ module.exports = function (Base, NAME, Constructor, next, DEFAULT, IS_SET, FORCE
 
 /***/ }),
 
-/***/ "31a1":
-/***/ (function(module, exports, __webpack_require__) {
-
-// extracted by mini-css-extract-plugin
-
-/***/ }),
-
 /***/ "32b3":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8139,17 +8132,6 @@ module.exports = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u20
 /***/ })
 
 /******/ })["default"];
-
-/***/ }),
-
-/***/ "4d98":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ImageInput_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("31a1");
-/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ImageInput_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ImageInput_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0__);
-/* unused harmony reexport * */
- /* unused harmony default export */ var _unused_webpack_default_export = (_node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ImageInput_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0___default.a); 
 
 /***/ }),
 
@@ -15532,12 +15514,12 @@ var AddresFormElementWrapper_component = Object(componentNormalizer["a" /* defau
 )
 
 /* harmony default export */ var AddresFormElementWrapper = (AddresFormElementWrapper_component.exports);
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"0d5ccda2-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/elements/ImageInput.vue?vue&type=template&id=2c9aa338&
-var ImageInputvue_type_template_id_2c9aa338_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"field image-input"},[_c('label',{staticClass:"label"},[_vm._v(_vm._s(_vm.schema.title))]),_c('div',{class:_vm.error.message?'control section input has-text-centered has-icons-right':'control section input has-text-centered'},[_c('image-uploader',{staticClass:"container",attrs:{"disabled":_vm.schema.disabled,"debug":1,"maxWidth":512,"quality":0.6,"autoRotate":true,"outputFormat":"string","preview":false,"className":['fileinput', { 'fileinput--loaded' : _vm.hasImage }],"capture":true,"accept":"image/*","doNotResize":"['gif', 'svg']"},on:{"input":_vm.setImage}},[_c('label',{attrs:{"slot":"upload-label","for":"fileInput"},slot:"upload-label"},[(_vm.image)?_c('picture',{style:('background-image:url('+_vm.image+')')}):_vm._e(),_c('small',[_vm._v(_vm._s(_vm.image?(_vm.byteLength(_vm.image) / 1000).toFixed(1) + " kb":""))]),_c('figure',[_c('svg',{attrs:{"xmlns":"http://www.w3.org/2000/svg","width":"32","height":"32","viewBox":"0 0 32 32"}},[_c('path',{staticClass:"path1",attrs:{"d":"M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z"}})])]),_c('span',{staticClass:"upload-caption"},[_vm._v(_vm._s(_vm.hasImage ? 'Reemplazar Imagen' : 'Subir Imagen'))])])])],1),_c('p',{class:_vm.error.message?'help is-danger':'help'},[_vm._v(_vm._s(_vm.error.message?_vm.error.message:_vm.schema.description))])])}
-var ImageInputvue_type_template_id_2c9aa338_staticRenderFns = []
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"0d5ccda2-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/elements/ImageInput.vue?vue&type=template&id=fdd34fd4&
+var ImageInputvue_type_template_id_fdd34fd4_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"field image-input"},[_c('label',{staticClass:"label"},[_vm._v(_vm._s(_vm.schema.title))]),_c('div',{class:_vm.error.message?'control section input has-text-centered has-icons-right':'control section input has-text-centered'},[_c('image-uploader',{staticClass:"container",attrs:{"disabled":_vm.schema.disabled,"debug":1,"maxWidth":512,"quality":0.6,"autoRotate":true,"outputFormat":"string","preview":false,"className":['fileinput', { 'fileinput--loaded' : _vm.hasImage }],"capture":true,"accept":"image/*","doNotResize":"['gif', 'svg']"},on:{"input":_vm.setImage}},[_c('label',{attrs:{"slot":"upload-label","for":"fileInput"},slot:"upload-label"},[(_vm.image)?_c('picture',{staticStyle:{"width":"100%","display":"block","padding-top":"100%","background-size":"cover","background-position":"center"},style:('background-image:url('+_vm.image+')')}):_vm._e(),_c('small',{staticStyle:{"font-size":".5em","text-align":"right","display":"block"}},[_vm._v(_vm._s(_vm.image?(_vm.byteLength(_vm.image) / 1000).toFixed(1) + " kb":""))]),_c('figure',[_c('svg',{attrs:{"xmlns":"http://www.w3.org/2000/svg","width":"32","height":"32","viewBox":"0 0 32 32"}},[_c('path',{staticClass:"path1",attrs:{"d":"M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z"}})])]),_c('span',{staticClass:"upload-caption"},[_vm._v(_vm._s(_vm.hasImage ? 'Reemplazar Imagen' : 'Subir Imagen'))])])])],1),_c('p',{class:_vm.error.message?'help is-danger':'help'},[_vm._v(_vm._s(_vm.error.message?_vm.error.message:_vm.schema.description))])])}
+var ImageInputvue_type_template_id_fdd34fd4_staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/elements/ImageInput.vue?vue&type=template&id=2c9aa338&
+// CONCATENATED MODULE: ./src/components/elements/ImageInput.vue?vue&type=template&id=fdd34fd4&
 
 // EXTERNAL MODULE: ./node_modules/vue-image-upload-resize/dist/vue-image-upload-resize.common.js
 var vue_image_upload_resize_common = __webpack_require__("4c7d");
@@ -15615,11 +15597,7 @@ var vue_image_upload_resize_common_default = /*#__PURE__*/__webpack_require__.n(
 });
 // CONCATENATED MODULE: ./src/components/elements/ImageInput.vue?vue&type=script&lang=js&
  /* harmony default export */ var elements_ImageInputvue_type_script_lang_js_ = (ImageInputvue_type_script_lang_js_); 
-// EXTERNAL MODULE: ./src/components/elements/ImageInput.vue?vue&type=style&index=0&lang=css&
-var ImageInputvue_type_style_index_0_lang_css_ = __webpack_require__("4d98");
-
 // CONCATENATED MODULE: ./src/components/elements/ImageInput.vue
-
 
 
 
@@ -15629,8 +15607,8 @@ var ImageInputvue_type_style_index_0_lang_css_ = __webpack_require__("4d98");
 
 var ImageInput_component = Object(componentNormalizer["a" /* default */])(
   elements_ImageInputvue_type_script_lang_js_,
-  ImageInputvue_type_template_id_2c9aa338_render,
-  ImageInputvue_type_template_id_2c9aa338_staticRenderFns,
+  ImageInputvue_type_template_id_fdd34fd4_render,
+  ImageInputvue_type_template_id_fdd34fd4_staticRenderFns,
   false,
   null,
   null,
--- src/components/elements/ImageInput.vue
+++ src/components/elements/ImageInput.vue
@@ -18,8 +18,8 @@
     @input="setImage"
   >
         <label for="fileInput" slot="upload-label">
-        <picture v-if="image" :style="'background-image:url('+image+')'"></picture>
-        <small>{{image?(byteLength(image) / 1000).toFixed(1) + " kb":""}}</small>
+        <picture v-if="image" :style="'background-image:url('+image+')'" style="width:100%;display: block;padding-top: 100%;background-size: cover; background-position: center;"></picture>
+        <small style="font-size: .5em;text-align: right;display: block;">{{image?(byteLength(image) / 1000).toFixed(1) + " kb":""}}</small>
         <figure>
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
               <path class="path1" d="M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z"></path>
@@ -73,26 +73,3 @@ export default {
   }
 }
 </script>
-<style>
-.image-input .control {
-  border: 1px solid #dbdbdb;
-  border-radius: 4px;
-  padding: 1em;
-  text-align: center;
-}
-.image-input picture{
-  width:100%;
-  display: block;
-  padding-top: 100%;
-  background-size: cover;
-  background-position: center;
-}
-.image-input picture+small{
-  font-size: .5em;
-  text-align: right;
-  display: block;
-}
-.image-input /deep/ #fileInput {
-  display:none;
-}
-</style>
