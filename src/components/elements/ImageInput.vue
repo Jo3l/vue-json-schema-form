e0acbd18d7b71d111dcc73e8dcec5a628c6bcc86
Jo3l <enriquito@gmail.com>
2020-11-20 15:53:23 +0100

    wip

--- src/components/elements/ImageInput.vue
+++ src/components/elements/ImageInput.vue
@@ -18,8 +18,8 @@
     @input="setImage"
   >
         <label for="fileInput" slot="upload-label">
-        <picture :style="'background-image:url('+image?image:'https://static.aiudo.es/img/aiudo-face.png'+')'" style="width:100%;display: block;padding-top: 100%;background-size: cover; background-position: center;"></picture>
-        <small style="font-size: .5em;text-align: right;display: block;">{{image?(byteLength(image) / 1000).toFixed(1) + " kb":""}}</small>
+        <picture :style="'background-image:url('+(image?image:'https://static.aiudo.es/img/aiudo-face.png')+')'" style="width:100%;display: block;padding-top: 100%;background-size: cover; background-position: center;"></picture>
+        <small v-if="image" style="font-size: .5em;text-align: right;display: block;">{{image?(byteLength(image) / 1000).toFixed(1) + " kb":""}}</small>
         <figure>
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
               <path class="path1" d="M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z"></path>
